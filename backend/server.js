1ce82425922b9cd5620e549dec079bef34d5b0f8
Kiran <kiranchauhan162k@gmail.com>
2024-09-10 12:48:37 +0530

    Initial commit

--- backend/server.js
+++ backend/server.js
@@ -7,19 +7,20 @@ import scriptCheckRoutes from './routes/scriptCheckRoutes.js';
 import authRoutes from './routes/authRoutes.js';
 import cors from 'cors';
 
-dotenv.config();
+// Resolve the correct path to the .env file in the root directory
+const __dirname = path.resolve();
+dotenv.config({ path: path.join(__dirname, '.env') });
 
 const app = express();
 app.use(express.json());
 
 // Add CORS middleware to allow requests from your domain
 app.use(cors({
-  origin: 'https://yourbot-fs1l.onrender.com', // Replace with your domain
+  origin: 'https://bot-rd1k.onrender.com', // Replace with your domain
   credentials: true,
 }));
 
 // Serve static files from the frontend
-const __dirname = path.resolve();
 app.use(express.static(path.join(__dirname, 'frontend/dist')));
 
 app.use('/api', registerRoutes);
@@ -39,30 +40,3 @@ app.get('*', (req, res) => {
 });
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-
-
-
-
-// import express from 'express';
-// import dotenv from 'dotenv';
-// import connectToMongoDB from './db/connectToMongoDB.js';
-// import registerRoutes from './routes/registerRoutes.js';
-// import scriptCheckRoutes from './routes/scriptCheckRoutes.js';
-// import authRoutes from './routes/authRoutes.js';
-
-// dotenv.config();
-
-// const app = express();
-// app.use(express.json());
-
-// app.use('/api', registerRoutes);
-// app.use('/api', scriptCheckRoutes);
-// app.use('/api', authRoutes);
-
-// // Connect to MongoDB
-// connectToMongoDB();
-
-// console.log('MONGODB_URI:', process.env.MONGO_DB_URI); // Add this line in server.js
-
-// const PORT = process.env.PORT || 5000;
-// app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
