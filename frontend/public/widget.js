cb4fa5f96c0a9dda0ecdbef819c2bf2032f92858
Kiran <kiranchauhan162k@gmail.com>
2024-09-12 18:00:42 +0530

    Initial commit

--- frontend/public/widget.js
+++ frontend/public/widget.js
@@ -12,7 +12,7 @@
    
     async function fetchAllowedDomain() {
         try {
-          const response = await fetch(`https://bot-rd1k.onrender.com/api/getdomainurl/${eid}`);
+          const response = await fetch(`${process.env.PRODUCTION_URL}/api/getdomainurl/${eid}`);
           const data = await response.json();
           console.log('Allowed domain fetched:', data.domainURL);
           return data.domainURL;
@@ -67,7 +67,7 @@
 
             // Load the chatbot logic script
             const chatbotScript = document.createElement('script');
-            chatbotScript.src = `https://bot-rd1k.onrender.com/chatbotLogic.js`; // Full URL for chatbotLogic.js
+            chatbotScript.src = `${process.env.PRODUCTION_URL}/chatbotLogic.js`; // Full URL for chatbotLogic.js
             chatbotScript.async = true;
             document.body.appendChild(chatbotScript);
 
