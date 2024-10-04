5525daefe7b27936a4eccd6b4bea8049f64557e7
Kiran <kiranchauhan162k@gmail.com>
2024-10-04 18:06:47 +0530

    Initial first Commit

--- backend/models/conversation.model.js
+++ backend/models/conversation.model.js
@@ -1,4 +1,10 @@
-//models/conversation.model.js
+import mongoose from 'mongoose';
+
+const messageSchema = new mongoose.Schema({
+  sender: { type: mongoose.Schema.Types.Mixed, required: true }, // Allows both ObjectId and string
+  text: { type: String, required: true },
+  createdAt: { type: Date, default: Date.now },
+});
 
 const conversationSchema = new mongoose.Schema({
   participants: [{ type: mongoose.Schema.Types.ObjectId, required: true }],
